c7dd9b17cd799b36211c923c8de681e5cca92a22
JoaquinGuzman <49413000+joacoguzmanz@users.noreply.github.com>
2022-08-04 10:51:46 -0300

    installed node and express

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const PORT = 3000;
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/index.html'));
+});
+
+app.get('/productCart', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/productCart.html'));
+});
+
+app.get('/productDetail', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/productDetail.html'));
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
+app.post('/newUser', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/newUser.html'));
+});

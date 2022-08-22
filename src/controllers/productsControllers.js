2eb105102af6464101ee69f62278170e42116388
JoaquinGuzman <49413000+joacoguzmanz@users.noreply.github.com>
2022-08-22 20:36:53 -0300

    debug

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -1,23 +1,26 @@
 const path = require('path');
 
 const productsControllers = {
-    productCart: function (req, res) {        
-       res.sendFile(path.resolve(__dirname, '../views/products/productCart.html'))
-      //  res.sendFile(cart, (err) => {
-      //   if (err) {
-      //     res.status(500).send('It looks like something crashed...');
-      //   }
-      // });
-    },
-    productDetail: function (req, res) {
-        res.sendFile(path.resolve(__dirname, '../views/products/productDetail.html'))        
-        // res.sendFile(details, (err) => {
-        //     if (err) {
-        //       res.status(500).send('It looks like something crashed...');
-        //     }
-        //   });
-    },
-    profile: ""
-}
+  productCart: function (req, res) {
+    // const cart = path.resolve(__dirname, '../views/products/productCart.html');
+    res.sendFile(path.resolve(__dirname, '../views/products/productCart.html'));
+    //  res.sendFile(cart, (err) => {
+    //   if (err) {
+    //     res.status(500).send('It looks like something crashed...');
+    //   }
+    // });
+  },
+  productDetail: function (req, res) {
+    res.sendFile(
+      path.resolve(__dirname, '../views/products/productDetail.html')
+    );
+    // res.sendFile(details, (err) => {
+    //     if (err) {
+    //       res.status(500).send('It looks like something crashed...');
+    //     }
+    //   });
+  },
+  profile: '',
+};
 
 module.exports = productsControllers;
